516723d4242cd4b6d4e1bf3b482645b28e853d76
Pascal Germroth <pascal@ensieve.org>
2013-06-17 18:19:59 +0200

    Replace thrust_algebra::for_eachN with GEN_FOR_EACH

--- boost/numeric/odeint/external/thrust/thrust_operations.hpp
+++ boost/numeric/odeint/external/thrust/thrust_operations.hpp
@@ -24,27 +24,10 @@ namespace odeint {
 
 #include <thrust/tuple.h>
 #include <thrust/iterator/zip_iterator.h>
-
-/**ToDo extend to scale_sum13 for rk78 */
+#include <boost/numeric/odeint/util/n_ary_helper.hpp>
 
 struct thrust_operations
 {
-    template< class Fac1 = double , class Fac2 = Fac1 >
-    struct scale_sum2
-    {
-        const Fac1 m_alpha1;
-        const Fac2 m_alpha2;
-
-        scale_sum2( const Fac1 alpha1 , const Fac2 alpha2 )
-        : m_alpha1( alpha1 ) , m_alpha2( alpha2 ) { }
-
-        template< class Tuple >
-        __host__ __device__
-        void operator()( Tuple t ) const
-        {
-            thrust::get<0>(t) = m_alpha1 * thrust::get<1>(t) + m_alpha2 * thrust::get<2>(t);
-        }
-    };
 
     template< class Fac1 = double , class Fac2 = Fac1 >
     struct scale_sum_swap2
@@ -65,140 +48,18 @@ struct thrust_operations
         }
     };
 
-    template< class Fac1 = double , class Fac2 = Fac1 , class Fac3 = Fac2 >
-    struct scale_sum3
-    {
-        const Fac1 m_alpha1;
-        const Fac2 m_alpha2;
-        const Fac3 m_alpha3;
 
-        scale_sum3( const Fac1 alpha1 , const Fac2 alpha2 , const Fac3 alpha3 )
-        : m_alpha1( alpha1 ) , m_alpha2( alpha2 ) , m_alpha3( alpha3 ) { }
-
-        template< class Tuple >
-        __host__ __device__
-        void operator()( Tuple t ) const
-        {
-            thrust::get<0>(t) = m_alpha1 * thrust::get<1>(t) +
-                    m_alpha2 * thrust::get<2>(t) +
-                    m_alpha3 * thrust::get<3>(t);
-        }
-    };
-
-
-    template< class Fac1 = double , class Fac2 = Fac1 , class Fac3 = Fac2 , class Fac4 = Fac3 >
-    struct scale_sum4
-    {
-        const Fac1 m_alpha1;
-        const Fac2 m_alpha2;
-        const Fac3 m_alpha3;
-        const Fac4 m_alpha4;
-
-        scale_sum4( const Fac1 alpha1 , const Fac2 alpha2 , const Fac3 alpha3 , const Fac4 alpha4 )
-        : m_alpha1( alpha1 ) , m_alpha2( alpha2 ) , m_alpha3( alpha3 ) , m_alpha4( alpha4 ){ }
-
-        template< class Tuple >
-        __host__ __device__
-        void operator()( Tuple t ) const
-        {
-            thrust::get<0>(t) = m_alpha1 * thrust::get<1>(t) +
-                    m_alpha2 * thrust::get<2>(t) +
-                    m_alpha3 * thrust::get<3>(t) +
-                    m_alpha4 * thrust::get<4>(t);
-        }
-    };
-
-
-    template< class Fac1 = double , class Fac2 = Fac1 , class Fac3 = Fac2 ,
-            class Fac4 = Fac3 , class Fac5 = Fac4 >
-    struct scale_sum5
-    {
-        const Fac1 m_alpha1;
-        const Fac2 m_alpha2;
-        const Fac3 m_alpha3;
-        const Fac4 m_alpha4;
-        const Fac5 m_alpha5;
-
-        scale_sum5( const Fac1 alpha1 , const Fac2 alpha2 , const Fac3 alpha3 ,
-                const Fac4 alpha4 , const Fac5 alpha5 )
-        : m_alpha1( alpha1 ) , m_alpha2( alpha2 ) , m_alpha3( alpha3 ) ,
-          m_alpha4( alpha4 ) , m_alpha5( alpha5 ) { }
-
-        template< class Tuple >
-        __host__ __device__
-        void operator()( Tuple t ) const
-        {
-            thrust::get<0>(t) = m_alpha1 * thrust::get<1>(t) +
-                    m_alpha2 * thrust::get<2>(t) +
-                    m_alpha3 * thrust::get<3>(t) +
-                    m_alpha4 * thrust::get<4>(t) +
-                    m_alpha5 * thrust::get<5>(t);
-        }
-    };
-
-
-    template< class Fac1 = double , class Fac2 = Fac1 , class Fac3 = Fac2 ,
-            class Fac4 = Fac3 , class Fac5 = Fac4 , class Fac6 = Fac5 >
-    struct scale_sum6
-    {
-        const Fac1 m_alpha1;
-        const Fac2 m_alpha2;
-        const Fac3 m_alpha3;
-        const Fac4 m_alpha4;
-        const Fac5 m_alpha5;
-        const Fac6 m_alpha6;
-
-        scale_sum6( const Fac1 alpha1 , const Fac2 alpha2 , const Fac3 alpha3 ,
-                const Fac4 alpha4 , const Fac5 alpha5 , const Fac6 alpha6 )
-        : m_alpha1( alpha1 ) , m_alpha2( alpha2 ) , m_alpha3( alpha3 ) ,
-          m_alpha4( alpha4 ) , m_alpha5( alpha5 ) , m_alpha6( alpha6 ) { }
-
-        template< class Tuple >
-        __host__ __device__
-        void operator()( Tuple t ) const
-        {
-            thrust::get<0>(t) = m_alpha1 * thrust::get<1>(t) +
-                    m_alpha2 * thrust::get<2>(t) +
-                    m_alpha3 * thrust::get<3>(t) +
-                    m_alpha4 * thrust::get<4>(t) +
-                    m_alpha5 * thrust::get<5>(t) +
-                    m_alpha6 * thrust::get<6>(t);
-        }
-    };
-
-
-    template< class Fac1 = double , class Fac2 = Fac1 , class Fac3 = Fac2 , class Fac4 = Fac3 ,
-            class Fac5 = Fac4 , class Fac6 = Fac5 , class Fac7 = Fac6 >
-    struct scale_sum7
-    {
-        const Fac1 m_alpha1;
-        const Fac2 m_alpha2;
-        const Fac3 m_alpha3;
-        const Fac4 m_alpha4;
-        const Fac5 m_alpha5;
-        const Fac6 m_alpha6;
-        const Fac7 m_alpha7;
-
-        scale_sum7( const Fac1 alpha1 , const Fac2 alpha2 , const Fac3 alpha3 ,
-                const Fac4 alpha4 , const Fac5 alpha5 , const Fac6 alpha6 , const Fac7 alpha7 )
-        : m_alpha1( alpha1 ) , m_alpha2( alpha2 ) , m_alpha3( alpha3 ) ,
-          m_alpha4( alpha4 ) , m_alpha5( alpha5 ) , m_alpha6( alpha6 ) , m_alpha7( alpha7 ) { }
-
-        template< class Tuple >
-        __host__ __device__
-        void operator()( Tuple t ) const
-        {
-            thrust::get<0>(t) = m_alpha1 * thrust::get<1>(t) +
-                    m_alpha2 * thrust::get<2>(t) +
-                    m_alpha3 * thrust::get<3>(t) +
-                    m_alpha4 * thrust::get<4>(t) +
-                    m_alpha5 * thrust::get<5>(t) +
-                    m_alpha6 * thrust::get<6>(t) +
-                    m_alpha7 * thrust::get<7>(t) ;
-        }
-    };
-
-
+#define BOOST_ODEINT_GEN_BODY(n, unused) \
+    template< class Tuple > \
+    __host__ __device__ \
+    void operator()( Tuple t ) const \
+    { thrust::get<0>(t) = BOOST_PP_REPEAT(n, BOOST_ODEINT_GEN_BODY_, ~) ; }
+#define BOOST_ODEINT_GEN_BODY_(z, n, unused) \
+    BOOST_PP_IF(n, +, BOOST_PP_EMPTY()) \
+    m_alpha##n * thrust::get<BOOST_PP_INC(n)>(t)
+BOOST_ODEINT_GEN_SCALE_SUM_OUTER((BOOST_ODEINT_GEN_BODY, ~))
+#undef BOOST_ODEINT_GEN_BODY
+#undef BOOST_ODEINT_GEN_BODY_
 
 
     template< class Fac1 = double >
